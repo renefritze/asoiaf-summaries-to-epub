b893cec3666adc1c79607fc06b3b1e9ffec8105a
René Fritze <rene.fritze@wwu.de>
2019-04-16 21:34:47 +0200

    use ebooklib instead of pypub

--- get.py
+++ get.py
@@ -2,7 +2,7 @@
 import os
 import requests
 from bs4 import BeautifulSoup
-import pypub
+from ebooklib import epub
 
 
 def parse(book, ch_count):
@@ -28,15 +28,51 @@ def parse(book, ch_count):
                 tags.append(tag)
         ctnt = '<h2>{}</h2>'.format(chapter)
         ctnt += '\n'.join('<p>{}</p>'.format(t.get_text()) for t in tags)
-        chapters.append(pypub.create_chapter_from_string(ctnt, title=chapter))
+        chapters.append((chapter, ctnt))
     return chapters
+
+
+def write_book(bookname, ch_count):
+    book = epub.EpubBook()
+
+    # set metadata
+    book.set_identifier('id123456')
+    book.set_title(bookname)
+    book.set_language('en')
+
+    book.add_author('G.R.R. Martin')
+
+    for idx, (chapter, ctnt) in enumerate(parse(book, ch_count)):
+        c1 = epub.EpubHtml(title=chapter, file_name=f'{chapter}.xhtml', lang='en')
+        c1.content = ctnt
+        book.add_item(c1)
+
+    # # define Table Of Contents
+    # book.toc = (epub.Link('chap_01.xhtml', 'Introduction', 'intro'),
+    #             (epub.Section('Simple book'),
+    #              (c1, ))
+    #             )
+
+    # add default NCX and Nav file
+    book.add_item(epub.EpubNcx())
+    book.add_item(epub.EpubNav())
+
+    # define CSS style
+    style = 'BODY {color: white;}'
+    nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
+
+    # add CSS file
+    book.add_item(nav_css)
+
+    # basic spine
+    book.spine = ['nav', c1]
+
+    # write to the file
+    epub.write_epub(f'{bookname}.epub', book, {})
 
 
 if __name__ == '__main__':
     books = [('A_Game_of_Thrones', 73), ('A_Clash_of_Kings', 70), ('A_Storm_of_Swords', 82), ('A_Feast_for_Crows', 46)
              , ('A_Dance_with_Dragons', 73)]
     for book, ch_count in books:
-        epub = pypub.Epub(title=book)
-        for chapter in parse(book, ch_count):
-            epub.add_chapter(chapter)
-        epub.create_epub(os.getcwd())
+        write_book(book, ch_count)
